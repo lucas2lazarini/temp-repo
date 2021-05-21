700dcd41ee2b2fc2c0c7f359cad72eeab1d5f7b3
Lucas Lazarini <lucas.lazarini.bdc@gmail.com>
2021-05-21 17:21:53 -0300

    Adicionei os arquivos do projeto que estavam em outra pasta

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export const PostCard = ({ title, cover, body, id }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h2>{id}</h2>
+      <h2> {title} </h2>
+      <p> {body} </p>
+    </div>
+  </div>
+);
